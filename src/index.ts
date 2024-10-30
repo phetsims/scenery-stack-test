5de9b84ba193c3c6a7747dbc100a913c9b1f9eea
Jonathan Olson <jonathan.olson@colorado.edu>
2024-10-29 19:03:55 -0600

    More inclusion

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * Exports for the 'phet' global
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
+/* eslint-disable */
+// @ts-nocheck
+
+import './preload';
+
+const phet = self.phet;
+export default phet;
